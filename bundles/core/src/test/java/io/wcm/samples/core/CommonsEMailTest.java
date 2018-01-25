2c33796406ef48b2f5dac7684ffd123bd98976c9
sseifert <sseifert@pro-vision.de>
2018-01-25 12:57:36 +0100

    add commons-email dependency and test case

---
+++ bundles/core/src/test/java/io/wcm/samples/core/CommonsEMailTest.java
@@ -0,0 +1,14 @@
+package io.wcm.samples.core;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import org.junit.Test;
+
+public class CommonsEMailTest {
+
+  @Test
+  public void testCommonsEMail() {
+    Email mail = new SimpleEmail();
+  }
+
+}
